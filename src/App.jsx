dba01191106a403903198499bd13860e564dedbe
alfrix <alfredomonclus@gmail.com>
2023-12-13 11:33:08 -0300

    usando una API

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 // Components
 import NavbarComponent from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer'
+import { useEffect, useState } from 'react'
+import { getProducts } from './services'
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
+const App = () => {
+
+  const [productsData, setProductsData] = useState([]);
+  
+  useEffect(() => {
+    getProducts()
+      .then((response) => {
+        setProductsData(response.data.products)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div class='contenedor-principal'>
+    <div className='contenedor-principal'>
       <NavbarComponent />
-      <div class='landing bg-dark text-dark-emphasis'>
-        <ItemListContainer greeting="Bienvenido" />
-      </div>
+      <ItemListContainer greeting="Bienvenido" productsData={productsData}/>
     </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+// import ProductCard from './ProductCard'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import './ItemListContainer.css'
+const descriptionLength = 60
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = ({ greeting, productsData }) => {
   return (
-    <h1 class="text-light">
-      {greeting}
-    </h1>
-  )
-}
+    <div>
+      {/* <h1 className='landing bg-dark text-dark-emphasis text-light'>
+        {greeting}
+      </h1> */}
+      <div className='contenedorProductos d-flex flex-wrap'>
+        {productsData.map((product) => {
+          return (
+            <Card key={product.id} className='col-lg-3 m-2'>
+              <Card.Img variant="top" src={product.thumbnail} className='imagen'/>
+              <Card.Body className='tarjeta'>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>{
+                  (product.description.length < descriptionLength)? product.description : product.description.slice(0, descriptionLength) + "..."
+                }</Card.Text>
+                <Card.Title>$ {product.price}  </Card.Title>
+                <Button variant="primary">Agregar al carrito</Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default ItemListContainer
